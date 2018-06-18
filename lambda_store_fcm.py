9a0be1c9f5223a1f923b9a6128d88c99aab9a308
gauri <gauri@pratilipi.com>
2018-06-18 18:46:48 +0530

    [init] lambda fns for storing fcm token

---
+++ lambda_store_fcm.py
@@ -0,0 +1,16 @@
+import boto3
+import time
+
+from boto3.dynamodb.conditions import Key, Attr
+
+def lambda_handler(event, context):
+    """
+    store fcm token for a user
+    """
+    print event
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('user_fcm_token_map')
+    item = {'user_id': event['user_id'], 'token': event['token'], 'last_updated': int(time.time())}
+    table.put_item(Item=item)
+    return "OK"
