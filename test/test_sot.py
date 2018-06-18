9775dd3e701e8deb18b4d49e5e175df55188386f
gauri <gauri@pratilipi.com>
2018-06-18 18:48:01 +0530

    [modify] testcases

--- test/test_sot.py
+++ test/test_sot.py
@@ -1,16 +1,28 @@
 import json
 import boto3
 import time
+import adjspecies
+import inspect
 
 from nose.tools import *
+from nose.tools import assert_raises
+from datetime import datetime
 from boto3.dynamodb.conditions import Key, Attr
+import botocore
 
 # How to run test
 # - nosetests test
 # - nosetests test.test_sot
 # - nosetests test.test_sot.test_sot_id_uniqness
 
-insert_table(table, sot_item())
+LANGUAGE = 'hi'
+USERID = 9090909090909090
+RUNTAG = adjspecies.random_adjspecies('_', 7)
+ACCESSTOKEN = 'test.sot.{}'.format(adjspecies.random_adjspecies('', 7))
+TESTTS = "%.10f" % time.time()
+
+def _getfname(fname):
+    return "{:<21}".format(fname[5:][:21])
 
 class test_sot():
     def __init__(self):
@@ -18,93 +30,87 @@ class test_sot():
         self.table = dynamodb.Table('sot')
 
     def sot_insert(self, item):
-        self.table.put_item(Item=item)
+        return self.table.put_item(Item=item)
 
     def sot_get(self, sot_id):
-        self.table.put_item(Item=item)
-        response = self.table.get_item( Key={ 'sot_id': sot_id })
-        item = response['Item']
-
-    def test_user_id_mandatory(self):
-        item = {}
-        self.insert(item)
+        response = self.table.get_item( Key={ 'sot_id': sot_id, 'user_id': USERID } )
+        return response['Item']
 
     """
-    def test_when_method_is_not_present(self):
-        language = 'hi'
-        user_id = 1234567890123456
-        access_token = '00000127-462a-42d2-b2b2-85639d47ee62'
-        ts = "%.10f" % time.time()
-        sot_id = '{}.{}.{}.{}'.format(language, access_token, user_id, ts[-5:])
+    def test_sot_id_length(self):
+        sot_id = '*'*100
+        item = { 'sot_id': sot_id, 'user_id': USERID }
+        response = self.sot_insert(item)
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
+    @raises(botocore.exceptions.ClientError)
+    def test_no_param(self):
+        item = {}
+        response = self.sot_insert(item)
+
+    @raises(botocore.exceptions.ClientError)
+    def test_user_id_mandatory(self):
+        fname = _getfname(inspect.stack()[0][3])
+        accesstoken = '{}.{}'.format(ACCESSTOKEN, fname)
+        ts = "%.10f" % time.time()
+        sot_id = '{}.{}.{}.{}'.format(LANGUAGE, accesstoken, USERID, ts[-5:])
+        item = { 'sot_id': sot_id, }
+        self.sot_insert(item)
+
+    @raises(botocore.exceptions.ClientError)
+    def test_sot_id_mandatory(self):
+        item = { 'user_id': USERID }
+        self.sot_insert(item)
+
+    def test_basic_valid_entry(self):
+        fname = _getfname(inspect.stack()[0][3])
+        accesstoken = '{}.{}'.format(ACCESSTOKEN, fname)
+        ts = "%.10f" % time.time()
+        sot_id = '{}.{}.{}.{}'.format(LANGUAGE, accesstoken, USERID, ts[-5:])
+        item = { 'sot_id': sot_id, 'user_id': USERID }
+        response = self.sot_insert(item)
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    def test_when_method_is_present(self):
+        fname = _getfname(inspect.stack()[0][3])
+        accesstoken = '{}.{}'.format(ACCESSTOKEN, fname)
+        ts = "%.10f" % time.time()
+        sot_id = '{}.{}.{}.{}'.format(LANGUAGE, accesstoken, USERID, ts[-5:])
+        item = { 'sot_id': sot_id, 'user_id': USERID, 'method': 'GET' }
+        resp =  self.sot_insert(item)
+        data = self.sot_get(sot_id)
+        assert data['method'] == 'GET'
+
+    def test_new_entry(self):
+        fname = _getfname(inspect.stack()[0][3])
+        accesstoken = '{}.{}'.format(ACCESSTOKEN, fname)
+        ts = "%.10f" % time.time()
+        sot_id = '{}.{}.{}.{}'.format(LANGUAGE, accesstoken, USERID, ts[-5:])
         item = {
             'sot_id': sot_id,
-            'user_id': user_id,
+            'user_id': USERID,
             'ts': int(time.time()),
             'version': '1.0',
-            'language': 'HINDI',
+            'language': LANGUAGE,
             'client': 'WEB',
             'method': 'GET',
             'path': '/api/pratilipi/list',
-            'headers': {'SOTTesting': ''},
+            'headers': {'User-Id': '2423423423423432'},
             'queryParams': {'_apiVer': '3', 'state': 'PUBLISHED'},
         }
-        return item
+        response = self.sot_insert(item)
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
-
-    def test_when_method_is_present(self):
-
-    def test_new_entry(self):
     def test_partition_key(self):
     def test_write_rate(self):
     def test_read_rate(self):
     def test_100M_sot_entries(self):
-    def test_indexex(self):
+    def test_indexes(self):
     def test_sot_id_uniqness(self):
-    def test_one_traceid_across_system(self):
-    def test_how_simple_it_is_to_query_based_on_sot_id(self):
-    def test_sot_version(self):
-    def test_language_mandatory(self):
-    def test_access_token_mandatory(self):
-            sot_id = '{}.{}.{}.{}'.format(language, access_token, user_id, ts[-5:])
-
-    def test_sot_id_length(self):
-    def test_method_support(self):
-    def test_when_headers_are_not_present(self):
-    def test_when_headers_are_there(self):
-    def test_when_query_param_is_not_there(self):
-    def test_when_query_param_is_present(self):
-    def test_when_path_is_not_present(self):
-    def test_when_path_is_present(self):
-    def test_all_ok(self):
-    def test_when_client_is_not_present(self):
-    def test_when_client_is_present(self):
-    def test_pag_request_count_eq_sot_count_for_a_day(self):
     def test_utf8_in_sot(self):
     def test_10gb_next_partition_limit(self):
-
-    def boundary conditions
-
-
-    def sot_item():
-        language = 'hi'
-        user_id = 1234567890123456
-        access_token = '00000127-462a-42d2-b2b2-85639d47ee62'
-        ts = "%.10f" % time.time()
-        sot_id = '{}.{}.{}.{}'.format(language, access_token, user_id, ts[-5:])
-
-        item = {
-            'sot_id': sot_id,
-            'user_id': user_id,
-            'ts': int(time.time()),
-            'version': '1.0',
-            'language': 'HINDI',
-            'client': 'WEB',
-            'method': 'GET',
-            'path': '/api/pratilipi/list',
-            'headers': {'SOTTesting': ''},
-            'queryParams': {'_apiVer': '3', 'state': 'PUBLISHED'},
-        }
-        return item
+    def test_pag_request_count_eq_sot_count_for_a_day(self):
+    def test_boundary_conditions(self):
 
     """
+
