a756c56de51edc9ddc37bd30717d725e080f7604
gauri <gauri@pratilipi.com>
2018-07-03 12:56:05 +0530

    [modify] prod compatible

--- utils/user_session.py
+++ utils/user_session.py
@@ -1,15 +1,18 @@
 import boto3
 
 from boto3.dynamodb.conditions import Key, Attr
-from utils import entity_parser, sender, user_tokens
 
 def put_item(user_id, entities):
     """
     build and store item for user session map
     """
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('user_session_map')
-    item = {'user_id': user_id}
-    item = dict(item.items() + entities.items())
-    table.put_item(Item=item)
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('user_session_map')
+        item = {'user_id': user_id}
+        item = dict(item.items() + entities.items())
+        table.put_item(Item=item)
+        print "added record to user_session_map, {}".format(item)
+    except Exception as err:
+        print "error:: while adding in user_session_map, {}".format(err)
     return
