78864ac26116acbfedfd6d18db3d95493827f1df
gauri <gauri@pratilipi.com>
2018-07-05 18:45:29 +0530

    [modify] reorganised

--- utils/message_formats.py
+++ utils/message_formats.py
@@ -7,7 +7,8 @@ APPSYNC['META'] = {
     'version': '1.0',
     'meta_entity': None,
     'meta_id': None,
-    'property': None
+    'property': None,
 }
-    
-#'meta': { 'entity': None, 'id': None, 'slug': None },
+
+
+
